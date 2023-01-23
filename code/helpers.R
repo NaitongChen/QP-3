83a775f179fd1a2697b458dab398dd41e16db8d0
chennaitong218@hotmail.com <chennaitong218@hotmail.com>
2023-01-23 14:41:02 -0800

    implemented all helpers

--- code/helpers.R
+++ code/helpers.R
@@ -41,22 +41,77 @@ variable_selection <- function(X, y) {
   n = nrow(X)
   p = ncol(X)
   cvfit <- cv.glmnet(X, drop(y), type.measure = "mse", family = "gaussian", 
-                     alpha = 1, nfolds = n, grouped = FALSE)
+                     alpha = 1, nfolds = n, grouped = FALSE, intercept = FALSE)
   final_fit <- glmnet(X, drop(y), type.measure = "mse", family = "gaussian", 
-                      alpha = 1, lambda = cvfit$lambda.1se)
+                      alpha = 1, lambda = cvfit$lambda.1se, grouped = FALSE, 
+                      intercept = FALSE)
   selected <- (1:p)[drop(final_fit$beta) != 0]
-  
+
   return(selected)
 }
 
-build_CI_split <- function(X, y, sigma_y, selected) {
-  # todo
+build_CI_split <- function(X, y, sigma_y, selected, alpha) {
+  X = X[, selected]
+  p = ncol(X)
+  n = nrow(X)
+  df = data.frame(X,y)
+
+  beta_hat = lm(y~.-1, data = df)$coefficients
+  cov = (solve(t(X) %*% X) %*% t(X) %*% (sigma_y^2 * diag(n)) 
+         %*% X %*% solve(t(X) %*% X))
+
+  z = qnorm(1-(alpha/2))
+
+  CIs = matrix(rep(0, p*2), nrow = p)
+
+  for (i in 1:p) {
+    CIs[i,1] = beta_hat[i] - z * cov[i,i]
+    CIs[i,2] = beta_hat[i] + z * cov[i,i]
+  }
+
+  return(beta_hat, CIs)
 }
 
-build_CI_1 <- function(X, y, tau, sigma_y, selected) {
-  # todo
+build_CI_1 <- function(X, y, tau, sigma_y, selected, alpha) {
+  X = X[, selected]
+  p = ncol(X)
+  n = nrow(X)
+  df = data.frame(X,y)
+  
+  beta_hat = lm(y~.-1, data = df)$coefficients
+  cov = (1 + tau^-2) * (solve(t(X) %*% X) %*% t(X) %*% (sigma_y^2 * diag(n)) 
+                        %*% X %*% solve(t(X) %*% X))
+  
+  z = qnorm(1-(alpha/2))
+  
+  CIs = matrix(rep(0, p*2), nrow = p)
+  
+  for (i in 1:p) {
+    CIs[i,1] = beta_hat[i] - z * cov[i,i]
+    CIs[i,2] = beta_hat[i] + z * cov[i,i]
+  }
+  
+  return(beta_hat, CIs)
 }
 
-build_CI_2 <- function(X, y, tau, sigma_y, selected) {
-  # todo
+build_CI_2 <- function(X, y, tau, sigma_y, selected, alpha, Z) {
+  X = X[, selected]
+  p = ncol(X)
+  n = nrow(X)
+  df = data.frame(X,y)
+  
+  beta_hat = lm(y~.-1, data = df)$coefficients
+  cov = (tau/(tau + 1)) * (solve(t(X) %*% X) %*% t(X) %*% (sigma_y^2 * diag(n)) 
+                           %*% X %*% solve(t(X) %*% X))
+  
+  z = qnorm(1-(alpha/2))
+  
+  CIs = matrix(rep(0, p*2), nrow = p)
+  
+  for (i in 1:p) {
+    CIs[i,1] = beta_hat[i] - z * cov[i,i]
+    CIs[i,2] = beta_hat[i] + z * cov[i,i]
+  }
+  
+  return(beta_hat, CIs)
 }
